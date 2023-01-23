ecc72527e4bd615014aaf345c5998d06ea174c81
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2023-01-23 19:24:08 +0100

    arcoiris

--- ejercicioJSUf3/circle.js
+++ ejercicioJSUf3/circle.js
@@ -34,6 +34,8 @@ function draw(ctx, length, x, y, color) {
   }
 
   function arco(ctx) {
+
+    /*Colores*/
     let color1 = 148;
     let color2 = 0;
     let color3 = 211;
@@ -63,27 +65,22 @@ function draw(ctx, length, x, y, color) {
     let color20 = 255;
     let color21 = 127;
     let color22 = 0;
-    let color23 = `rgb(${color1},${color2},${color3})`
+    let color23 = `rgb(${color20},${color21},${color22})`
 
     let color24 = 255;
     let color25 = 0;
     let color26 = 0;
-    let color27 = `rgb(${color1},${color2},${color3})`
+    let color27 = `rgb(${color24},${color25},${color26})`
 
     let arrayColors = [color, color7, color11, color15, color19, color23, color27] 
-
+    let a = 320;
     for (let i = 0; i < 7; i++){
         ctx.strokeStyle = arrayColors[i];
         ctx.lineWidth = 10;
         ctx.beginPath();
-        if (i == 0){
-            ctx.arc(225, 320, 100, Math.PI,0);
-            
-        }
-        else if(i == 1){  
-            ctx.arc(225, 310, 100, Math.PI,0);
-        }
+        ctx.arc(225, a, 100, Math.PI,0);  
         ctx.stroke();
+        a = a - 10;
     }
 
   
@@ -92,114 +89,6 @@ function draw(ctx, length, x, y, color) {
     };
   }
 
-  /* function arco2(ctx) {
-    let color1 = 75;
-    let color2 = 0;
-    let color3 = 130;
-    let color = `rgb(${color1},${color2},${color3})`
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 10;
-  
-    ctx.beginPath();
-    ctx.arc(225, 310, 100, Math.PI,0);
-    ctx.stroke();
-  
-    return {
-      color: color
-    };
-    
-  } 
-
-  function arco3(ctx) {
-    let color1 = 0;
-    let color2 = 0;
-    let color3 = 255;
-    let color = `rgb(${color1},${color2},${color3})`
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 10;
-  
-    ctx.beginPath();
-    ctx.arc(225, 300, 100, Math.PI,0);
-    ctx.stroke();
-  
-    return {
-      color: color
-    };
-    
-  }
-
-  function arco4(ctx) {
-    let color1 = 0;
-    let color2 = 255;
-    let color3 = 0;
-    let color = `rgb(${color1},${color2},${color3})`
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 10;
-  
-    ctx.beginPath();
-    ctx.arc(225, 290, 100, Math.PI,0);
-    ctx.stroke();
-  
-    return {
-      color: color
-    };
-    
-  }
-
-  function arco5(ctx) {
-    let color1 = 255;
-    let color2 = 255;
-    let color3 = 0;
-    let color = `rgb(${color1},${color2},${color3})`
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 10;
-  
-    ctx.beginPath();
-    ctx.arc(225, 280, 100, Math.PI,0);
-    ctx.stroke();
-  
-    return {
-      color: color
-    };
-    
-  }
-
-  function arco6(ctx) {
-    let color1 = 255;
-    let color2 = 127;
-    let color3 = 0;
-    let color = `rgb(${color1},${color2},${color3})`
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 10;
-  
-    ctx.beginPath();
-    ctx.arc(225, 270, 100, Math.PI,0);
-    ctx.stroke();
-  
-    return {
-      color: color
-    };
-    
-  }
-
-  function arco7(ctx) {
-    let color1 = 255;
-    let color2 = 0;
-    let color3 = 0;
-    let color = `rgb(${color1},${color2},${color3})`
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 10;
-  
-    ctx.beginPath();
-    ctx.arc(225, 260, 100, Math.PI,0);
-    ctx.stroke();
-  
-    return {
-      color: color
-    };
-    
-  }
-*/
 export { name, draw, reportArea, reportPerimeter, arco};
 //export default arco;
 
